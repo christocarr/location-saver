c6bfd49eff4538d0d0983bd40494ff5e4940f78b
christocarr <christocarr@outlook.com>
2021-02-03 18:33:43 +0000

    Add class for new places objects when user creates a new place

---
+++ models/place.js
@@ -0,0 +1,10 @@
+import { exp } from 'react-native/Libraries/Animated/src/Easing';
+
+class Place {
+  constructor(id, title) {
+    this.id = id;
+    this.title = title;
+  }
+}
+
+export default Place;
