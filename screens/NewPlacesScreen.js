11af44138f7ead5da44d1cd9464435e647ed1555
christocarr <christocarr@outlook.com>
2021-02-03 18:13:19 +0000

    Add state to handle text input

--- screens/NewPlacesScreen.js
+++ screens/NewPlacesScreen.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  View,
+  TextInput,
+  Button,
+  Text,
+  StyleSheet,
+} from 'react-native';
 
-function NewPlacesScreen() {
+function NewPlacesScreen(props) {
+  const [titleValue, setTitleValue] = useState('');
+
+  const handleInputChange = (text) => {
+    console.log(text);
+    setTitleValue(text);
+  };
+
   return (
-    <View>
-      <Text>New Places Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={handleInputChange}
+          value={titleValue}
+        />
+        <Button title="Add" />
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 20,
+  },
+});
 
 export default NewPlacesScreen;
